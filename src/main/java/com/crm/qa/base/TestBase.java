7694625b07240669eb1b14279ae7da24d0e57a9d
Racha Hamdi <rachahamdii@gmail.com>
2019-10-21 23:08:06 +0200

    drivers location

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,11 +41,11 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");	
+			System.setProperty("webdriver.chrome.driver", "F:/Workspace-EclipseOxygen/drivers/chromedriver.exe");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
-			System.setProperty("webdriver.gecko.driver", "/Users/naveenkhunteta/Documents/SeleniumServer/geckodriver");	
+			System.setProperty("webdriver.gecko.driver", "F:/Workspace-EclipseOxygen/drivers/geckodriver.exe");	
 			driver = new FirefoxDriver(); 
 		}
 		
